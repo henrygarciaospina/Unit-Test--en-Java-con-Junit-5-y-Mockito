ff9cff76eb38b2e60a1aadd1d008ba17ed59f267
henrygosling <henrygarciaospina@hotmail.com>
2022-08-11 10:28:44 -0500

    22. Añadiendo métodos a Calculator

--- src/main/java/com/example/unitest/unitest/junit5/Calculator.java
+++ src/main/java/com/example/unitest/unitest/junit5/Calculator.java
@@ -13,4 +13,17 @@ public class Calculator {
         result = n1 - n2;
         return result;
     }
+
+    public int divided(int n1, int n2){
+        result = n1 / n2;
+        return result;
+    }
+
+    public int dividedByZero(int n1, int n2){
+        if(n2 == 0){
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+        result = n1 / n2;
+        return result;
+    }
 }
